0eb80b851f71c1b144e1e2d68a3664d85d34b2fd
Jedrek11 <jedrekpawlowski011@gmail.com>
2023-04-21 12:50:03 +0200

    2023-04-21 JP

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # CeneoScraper
+## CeneoScraper
+| składowa | nazwa | selektor |
+| --- | --- | --- |
+| opinia | opinion/single\_opinion | div.js\_product-review |
+| identyfikator opinii | opinion\_id | [data-entry-id] |
+| autor | author | span.user-post\_\_author-name |
+| rekomendacja | recommendation | span.user-post\_\_author-recomendation \> em |
+| liczba gwiazdek | stars | span.user-post\_\_score-countspan.score-marker[style] |
+| czy opinia jest potwierdzona zakupem | purchased | div.review-pz |
+| data wystawienia opinii | opinion\_date | span.user-post\_\_published \> time:nth\_child(1)[datetime] |
+| data zakupu produktu | purchase\_date | span.user-post\_\_published \> time:nth\_child(2)[datetime] |
+| ile osób uznało opinię za przydatną | usefull\_count | button.vote-yes[data-total-vote]button.vote-yes \> span |
+| ile osób uznało opinię za nieprzydatną | unusefull\_count | button.vote-no[data-total-vote]button.vote-no \> span |
+| treść opinii | content | div.user-post\_\_text |
+| listę zalet | pros | div.review-feature\_\_title--positives ~ div.review
--- scraper.py
+++ scraper.py
@@ -1,39 +1,46 @@
 import requests
 from bs4 import BeautifulSoup
-product_code = input("podaj kod produktu: ")
-# url = "https://www.ceneo.pl/" + product_code +"#tab=reviews"
 
-url = f"https://www.ceneo.pl/{product_code}#tab=reviews"
-respons = requests.get(url)
-# print(respons.status_code)
-if respons.status_code == requests.codes.ok:
-    page_dom =  BeautifulSoup(respons.text, 'html.parser')
-    opinions = page_dom.select("div.js_product-review")
-    if len(opinions) > 0:
+
+# product_code = input("Podaj kot produktu: ")
+product_code = '58835954'
+
+
+URL = f"https://www.ceneo.pl/{product_code}#tab=reviews"
+res = requests.get(URL)
+
+
+if res.status_code == requests.codes.ok:
+    page_DOM = BeautifulSoup(res.text, 'html.parser')
+    opinions_DOM = page_DOM.select("div.js_product-review")
+    
+    if len(opinions_DOM) > 0:
         opinions_all = []
-        for opinons in opinions:
+        print(opinions_DOM)
+        for opinion in opinions_DOM:
             single_opinion = {
-                "opinion_id": ""        ,   #opinion.dataset.entry-id="14616342"
-                "author": ""            ,   #.user-post__author-name
-                "recomedation": ""      ,   #.recomended
-                "stars": ""             ,   #.score-maker[style]
-                "purchased": ""         ,   #.review-pz[title]
-                "opinion_date": ""      ,   #.user-post__published:first-child[datetime]
-                "purchase_date": ""     ,   #.user-post__published:last-child[datatime]
-                "usefull_count": ""     ,   #.vote-yes[data-vote]
-                "unusefull_count":""    ,   #.vote-yes[data-vote]
-                "content": ""           ,   #.user-post__text
-                "pros": ""              ,   #review-feature__col
-                "cons": ""                  #review-feature__col
+                "opinion_id": opinion["data-entry-id"],
+                "author": opinion.select_one("span.user-post__author-name").get_text().strip(),
+                "recommendation": opinion.select_one("span.user-post__author-recomendation > em").get_text().strip(), 
+                "stars": opinion.select_one("span.user-post__score-count").get_text().strip(),
+                "purchased": opinion.select_one("div.review-pz").get_text().strip(),
+                "opinion_date": opinion.select_one("span.user-post__published > time:nth-child(1)")['datetime'].strip(),
+                "purchase_date": opinion.select_one("span.user-post__published > time:nth-child(2)")['datetime'].strip(),
+                "usefull_count": opinion.select_one("button.vote-yes")["data-total-vote"].strip(),
+                "unusefull_count": opinion.select_one("button.vote-no")["data-total-vote"].strip(),
+                "content": opinion.select_one("div.user-post__text").get_text().strip(),
+                "pros": [p.get_text().strip() for p in opinion.select("div.review-feature__title--positives ~ div.review-feature__item")],
+                "cons": [p.get_text().strip() for p in opinion.select("div.review-feature__title--negatives ~ div.review-feature__item")] 
             }
+        opinions_all.append(single_opinion)
+        print(opinions_all)
     else:
         print("Nie ma opinii")
+ 
+
+    # else:
+    # print("Nie ma opinii")
         
-    # print(page_dom.title.string)
-
-
-
-
 
 # kod: 58835954
 # r.status_code == request.codes.ok
