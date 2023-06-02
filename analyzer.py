a6a0bda3cf8f81fe5759f3976acf6d1590c18a9e
Jedrek11 <jedrekpawlowski011@gmail.com>
2023-06-02 12:59:15 +0200

    2023-06-02 JP

--- analyzer.py
+++ analyzer.py
@@ -1,6 +1,12 @@
 import os
 import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
 
+try:
+    os.mkdir("charts")
+except FileExistsError:
+   pass
 # print([*filename.removesuffix(".json") for filename in os.listdir("opinions")])
 print(*list(map(lambda x: x.removesuffix(".json"), os.listdir("opinions"))), sep="\n")
 product_code = input("Podaj kot produktu: ")
@@ -11,12 +17,48 @@ opinions.stars = opinions.stars.map(lambda x: float(x.split("/")[0].replace(",",
 stats = {
     'opinions_count': len(opinions),
     # 'opinions_count': opinions.shape[0],
-    'pros_count':  opinions.pros.map(bool).sum(),
-    'cons_count':  opinions.cons.map(bool).sum(),
-    'average_score': opinions.stars.mean()
+    'pros_count':  int(opinions.pros.map(bool).sum()),
+    'cons_count':  int(opinions.cons.map(bool).sum()),
+    'average_score': float(opinions.stars.mean())
 }
 print(f"""Dla produktu o indetyfikatorze {product_code}
 pobrano {stats["opinions_count"]} opinii.
 Dla {stats["pros_count"]} opinii podana została lista zalet produktu,
 a dla {stats["cons_count"]} opinii podana została lista jego wad.
 Średnia ocena produktu wynosi {stats["average_score"]:.2f}.""")
+
+colors_stats = {}
+for i in np.arange(0,5.5,0.5):
+    colors_stats[i] = "crimson" if i <= 2.5 else "steelblue" if i <= 3.5 else "forestgreen"
+stars = opinions.stars.value_counts().reindex(list(np.arange(0,5.5,0.5)), fill_value=0)
+print(stars)
+stars.plot.bar(color=colors_stats.values())
+plt.xticks(rotation='horizontal')
+plt.title("Rozkład liczby gwiazdek w opinii konsumentów")
+plt.xlabel("liczby gwiazdek")
+plt.ylabel("liczba opinii")
+plt.ylim(0,max(stars)+10)
+for index, value in enumerate(stars):
+    plt.text(index, value+0.5, str(value), ha="center")
+# plt.show()
+plt.savefig(f"charts/{product_code}_stars.png")
+plt.close()
+
+recommendation = opinions["recommendation"].value_counts(dropna= False).reindex(["Polecam","Nie Polecam",None], fill_value=0)
+recommendation.plot.pie(
+    lable="",
+    autopct= lambda p: '{:.1f}%'.format(round(p)) if p > 0 else '',
+    lables = ["Polecam","Nie Polecam","Nie mam zdania"],
+    colors = ["forestgreen","crismon","steelblue"]
+)
+plt.legend(loc='upper center',ncol=3)
+plt.title("Rozkład rekomendacji w oppinii konsumentów")
+plt.savefig(f"./charts/{product_code}_recommendation.png")
+plt.close()
+
+stats['stars'] = stars.to_dict()
+stats['recommendation'] = recommendation.to_dict()
+
+print(stats)
+with open(f"stats/{product_code}.json", "w" encoding="UTF-8") as jf:
+    json.dump(stats, jf, indent=4, ensure_ascii=False)
